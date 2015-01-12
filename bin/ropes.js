545c6164ba09fef854dc0cf12f841ca28f3b2abf
Christopher WJ Rueber <crueber@gmail.com>
2015-01-12 13:56:04 -0600

    bump

--- bin/ropes.js
+++ bin/ropes.js
@@ -5,7 +5,7 @@
 var ropes = require('commander');
 
 ropes
-  .version('0.1.0')
+  .version('0.1.1')
   .command('weave [app-name]', 'pulls in ropes for you to alter')
   .command('braid [name] [weave-location]', 'creates a new app based on a particular weave')
   .parse(process.argv);
